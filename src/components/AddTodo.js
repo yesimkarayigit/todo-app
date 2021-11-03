2e7b82907637edff8459628f23cf5c69ee757421
Yesim Karayigit <yesim.karayigit@gmail.com>
2021-11-03 15:29:59 +0300

    Refactoring & improvement on adding todo

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -5,6 +5,20 @@ import { addTodo } from '../store/actions';
 const AddTodo = (props) => {
   const [text, setText] = useState('');
 
+  const addingTodo = () => {
+    if (!text && text.length < 1) return;
+
+    setText('');
+    props.addTodo(text);
+  }
+
+
+  const handleKeypress = e => {
+    if (e.charCode === 13) {
+      addingTodo();
+    }
+  };
+
   return (
     <div className="add-todo-wrapper">
       <input 
@@ -12,20 +26,11 @@ const AddTodo = (props) => {
         value={text} 
         onChange={e => setText(e.target.value)}
         className="add-todo-input"
+        onKeyPress={handleKeypress}
       />
-      <button className="add-todo-button" onClick={() => {
-        if (!text && text.length < 1) return;
-
-        setText('')
-        props.addTodo(text)} }>Add</button>
+      <button className="add-todo-button" onClick={addingTodo}>Add</button>
     </div>
   )
 }
 
-const mapStateToProps = state => {
-  return {
-    list: state.list,
-  }
-}
-
-export default connect(mapStateToProps, { addTodo })(AddTodo);
+export default connect(null, { addTodo })(AddTodo);
